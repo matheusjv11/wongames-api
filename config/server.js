6ff428a0cf0a38377e1b9894f38495f0f7701bc9
Matheus Almeida <matheusjv14@gmail.com>
2021-02-18 10:18:01 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'eb261d3c7953c431946185d7e98d0d84'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '206e217c-97b3-4c43-98c9-dfb42fb43e45'
+};
